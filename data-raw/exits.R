39aa42c974af1ac0bd6ad8938f0670b4020015c6
sccmckenzie <sccmckenzie@gmail.com>
2021-02-25 20:11:31 -0600

    add janitor to exits

--- data-raw/exits.R
+++ data-raw/exits.R
@@ -6,10 +6,11 @@ exits <- read_html("https://en.wikipedia.org/wiki/Texas_State_Highway_Loop_1") %
   .[[5]] %>%
   html_table(fill = TRUE) %>%
   as_tibble() %>%
-  select(mi:Notes) %>%
+  janitor::clean_names() %>%
+  select(mi:notes) %>%
   slice(-n()) %>%
   mutate(across(mi:km, as.double),
-         Notes = if_else(stringr::str_length(Notes) == 0, NA_character_, Notes)) %>%
+         notes = if_else(stringr::str_length(notes) == 0, NA_character_, notes)) %>%
   filter(!is.na(mi))
 
 usethis::use_data(exits, overwrite = TRUE)
